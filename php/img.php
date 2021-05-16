caca325a8e7cbe222d9cab3b667f7eb248697c9d
PanfletoIG <panfletoindiegames@gmail.com>
2021-05-16 16:56:51 -0600

    Actualizacion 0.0.1

--- php/img.php
+++ php/img.php
@@ -1,20 +1,18 @@
 <?php
-    $nombre=$_FILES['archivo']['name'];
-    $guardado=$_FILES['archivo']['tmp_name'];
+    $nombre=$_FILES['imagen']['name'];
+    $guardado=$_FILES['imagen']['tmp_name'];
 
-    if(!file_exists('img')){
-        mkdir('img',0777,true);
-        if(file_exists('img')){
-            if(move_uploaded_file($guardado, "img".$nombre)){
+    if(!file_exists('../img/')){
+        mkdir('../img/',0777,true);
+        if(file_exists('../img/')){
+            if(move_uploaded_file($guardado, "../img/".$nombre)){
                 echo "Archivo Guardado con exito";
             }else{
                 echo "Archivo No se guardo :(";
             }
         }
-    }
-    else
-    {
-        if(move_uploaded_file($guardado, "img".$nombre)){
+    }else{
+        if(move_uploaded_file($guardado, "../img/".$nombre)){
             echo "Archivo Guardado con exito";
         }else{
             echo "Archivo No se guardo :(";
