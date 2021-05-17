a4bf87055c561079a125531c8aaf0e829fb4618d
PanfletoIG <panfletoindiegames@gmail.com>
2021-05-16 19:29:02 -0600

    Actualizacion 0.0.1

--- php/img.php
+++ php/img.php
@@ -1,21 +1,32 @@
 <?php
+    print_r($_FILES);
     $nombre=$_FILES['imagen']['name'];
     $guardado=$_FILES['imagen']['tmp_name'];
+    $size=$_FILES['imagen']['size'];
+    $type=$_FILES['imagen']['type'];
 
-    if(!file_exists('../img/')){
-        mkdir('../img/',0777,true);
-        if(file_exists('../img/')){
-            if(move_uploaded_file($guardado, "../img/".$nombre)){
-                echo "Archivo Guardado con exito";
+    if($type=='image/jpg' || $type=='image/JPG' || $type=='image/jpeg'){
+        if($size > 5*1024*1024){
+            echo "Error: El archivo es demasiado grande soporto 5MB";
+        }else{
+            if(!file_exists('../img/')){
+                mkdir('../img/',0777,true);
+                if(file_exists('../img/')){
+                    if(move_uploaded_file($guardado, "../img/".$nombre)){
+                        echo "Archivo Guardado con exito";
+                    }else{
+                        echo "Archivo No se guardo :(";
+                    }
+                }
             }else{
-                echo "Archivo No se guardo :(";
+                if(move_uploaded_file($guardado, "../img/".$nombre)){
+                    echo "Archivo Guardado con exito";
+                }else{
+                    echo "Archivo No se guardo :(";
+                }
             }
         }
     }else{
-        if(move_uploaded_file($guardado, "../img/".$nombre)){
-            echo "Archivo Guardado con exito";
-        }else{
-            echo "Archivo No se guardo :(";
-        }
+        echo "Error: Archivo no compatible :(";
     }
 ?>
